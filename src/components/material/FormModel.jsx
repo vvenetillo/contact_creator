46774a4fec9f7309dfbe176e8c07b93821a1db2c
vvenetillo <venetillovictor@gmail.com>
2024-01-09 20:28:54 -0300

    inser userlist and backend

--- src/components/material/FormModel.jsx
+++ src/components/material/FormModel.jsx
@@ -3,11 +3,12 @@ import { useReducer } from "react";
 import styled from "styled-components";
 import Swal from "sweetalert2";
 import { useNavigate } from "react-router-dom";
-import axios from 'axios'
+import('dotenv').config()
+import axios from "axios";
 
-axios.create({
-  baseURL: 'http://localhost:3001/'
-})
+const api = axios.create({
+  baseURL: import.meta.env.BASE_URL,
+});
 
 import photo from "../../assets/photo.svg";
 
@@ -72,8 +73,8 @@ const ErrorMessage = styled.p`
 
 const initialState = {
   username: "",
-  name: "",
-  password: "",
+  email: "",
+  telefone: "",
   errorMessage: "",
   submittedData: [],
   showList: false,
@@ -83,10 +84,10 @@ const reducer = (state, action) => {
   switch (action.type) {
     case "SET_USERNAME":
       return { ...state, username: action.payload };
-    case "SET_NAME":
-      return { ...state, name: action.payload };
-    case "SET_PASSWORD":
-      return { ...state, password: action.payload };
+    case "SET_EMAIL":
+      return { ...state, email: action.payload };
+    case "SET_TELEFONE":
+      return { ...state, telefone: action.payload };
     case "SET_ERROR_MESSAGE":
       return { ...state, errorMessage: action.payload };
     case "SET_SUBMITTED_DATA":
@@ -98,33 +99,50 @@ const reducer = (state, action) => {
   }
 };
 
-const Login = () => {
+export default function Login() {
   const [state, dispatch] = useReducer(reducer, initialState);
   const navigate = useNavigate();
 
-  const handleLogin = async (e) => {
-    e.preventDefault();
-
-    const { username, name, password } = state;
-
-    if (!username || !name || !password) {
-      dispatch({ type: "SET_ERROR_MESSAGE", payload: "Preencha corretamente os dados" });
-    } else {
-      const formData = [name, username, password];
-      dispatch({ type: "SET_SUBMITTED_DATA", payload: formData });
-      dispatch({ type: "SET_SHOW_LIST", payload: true });
-
-      // Chama a função de retorno do componente pai
-      console.log(formData)
-
+  const handlesubmit = () => {
+    const sweet = () =>
       Swal.fire({
-        title: "Good job!",
-        text: "You clicked the button!",
+        title: "Muito bem!",
+        text: "Cadastrado com sucesso!",
         icon: "success",
       });
 
-      // Navegar para outra página (opcional)
-      navigate("/list");
+    sweet();
+  };
+
+  const handleLogin = async (e) => {
+    e.preventDefault();
+    const { username, email, telefone } = state;
+    const formData = { username, email, telefone };
+
+    console.log(formData);
+
+    try {
+      const response = 
+      await api
+      .post("/register", formData);
+
+      if (response.status === 201) {
+        console.log(response.data.msg); // Mensagem do backend
+        console.log(response.data.user); // Dados do usuário cadastrado
+        dispatch({ type: "SET_SUBMITTED_DATA", payload: formData });
+        dispatch({ type: "SET_SHOW_LIST", payload: true });
+        dispatch({ type: "SET_USERNAME", payload: "" });
+        dispatch({ type: "SET_EMAIL", payload: "" });
+        dispatch({ type: "SET_TELEFONE", payload: "" });
+        handlesubmit();
+        navigate("/list");
+      }
+    } catch (error) {
+      console.error("Erro ao enviar o formulário:", error);
+      dispatch({
+        type: "SET_ERROR_MESSAGE",
+        payload: "Preencha corretamente os dados",
+      });
     }
   };
 
@@ -136,11 +154,13 @@ const Login = () => {
           <Label>Nome Completo</Label>
           <Input
             type="text"
-            name="name"
-            id="fname"
+            name="username"
+            id="username"
             required
-            value={state.name}
-            onChange={(e) => dispatch({ type: "SET_NAME", payload: e.target.value })}
+            value={state.username}
+            onChange={(e) =>
+              dispatch({ type: "SET_USERNAME", payload: e.target.value })
+            }
           />
         </FormGroup>
         <FormGroup>
@@ -148,28 +168,34 @@ const Login = () => {
           <Input
             type="text"
             name="email"
-            id="femail"
+            id="email"
             required
             value={state.email}
-            onChange={(e) => dispatch({ type: "SET_EMAIL", payload: e.target.value })}
+            onChange={(e) =>
+              dispatch({ type: "SET_EMAIL", payload: e.target.value })
+            }
           />
         </FormGroup>
         <FormGroup>
           <Label>Telefone</Label>
           <Input
-            type="text"
+            type="tel"
             name="telefone"
-            id="ftelefone"
+            id="telefone"
             required
             value={state.telefone}
-            onChange={(e) => dispatch({ type: "SET_TELEFONE", payload: e.target.value })}
+            onChange={(e) =>
+              dispatch({ type: "SET_TELEFONE", payload: e.target.value })
+            }
           />
         </FormGroup>
-        <SubmitButton type="submit" onSubmit={handleLogin}>ENVIAR</SubmitButton>
-        {state.errorMessage && <ErrorMessage>{state.errorMessage}</ErrorMessage>}
+        <SubmitButton type="submit" onSubmit={handlesubmit}>
+          ENVIAR
+        </SubmitButton>
+        {state.errorMessage && (
+          <ErrorMessage>{state.errorMessage}</ErrorMessage>
+        )}
       </Form>
     </Container>
   );
-};
-
-export default Login;
+}
